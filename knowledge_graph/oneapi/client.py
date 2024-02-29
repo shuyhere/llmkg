fd49a80b9430dadfb921a37cc5c072b821e14345
shu <shuyhere@163.com>
2024-02-29 15:31:20 +0800

    graphdb

--- knowledge_graph/oneapi/client.py
+++ knowledge_graph/oneapi/client.py
@@ -1,14 +1,14 @@
 from openai import OpenAI
 
-client = OpenAI(
-    api_key="EMPTY",
-    base_url="http://127.0.0.1:8000/v1/",
-)
-
 # client = OpenAI(
-#     api_key="sk-",
-# 	base_url="https://aigptx.top/v1/",
+#     api_key="EMPTY",
+#     base_url="http://127.0.0.1:8000/v1/",
 # )
+
+client = OpenAI(
+    api_key="",
+	base_url="https://aigptx.top/v1/",
+)
 
 
 def generate(model_name, prompt, system=None, template=None, context=None, options=None, callback=None):
